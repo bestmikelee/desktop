01bcd6dbc19a24b87ca53df7a1aeb92abbe86b9d
Roger Beaman <rwcbeaman@gmail.com>
2015-11-22 03:40:22 -0500

    makeover finally done

---
+++ browser/app/dashboard/buildingAccordionAside/buildingAccordionAside.js
@@ -0,0 +1,6 @@
+app.directive('buildingAccordionAside', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'app/dashboard/buildingAccordionAside/buildingAccordionAside.html'
+	};
+});
---
+++ browser/app/dashboard/notificationsAside/notificationsAside.js
@@ -0,0 +1,6 @@
+app.directive('notificationsAside', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'app/dashboard/notificationsAside/notificationsAside.html'
+	};
+});
